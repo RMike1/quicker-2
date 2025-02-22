620b143621526320f74b82118e9f6a202b0f5b5f
RMike1 <71224316+RMike1@users.noreply.github.com>
2025-02-22 13:31:29 +0200

    ..

---
+++ app/Http/Controllers/Home/HomeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+class HomeController extends Controller
+{
+    public function index()
+    {
+       return Inertia::render('Dashboard',[
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+       ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,22 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\Home\HomeController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
-Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+        
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+// Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+Route::middleware([config('jetstream.auth_session'),])->group(function () {
+    Route::get('/', [HomeController::class,'index'])->name('user.index');
 });
 
 Route::get('/verify-email', function () {
